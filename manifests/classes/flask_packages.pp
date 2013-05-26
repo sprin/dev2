08d7ad03339659b49680b68dee7fa5497f124b01
sprin <sprin>
2013-05-26 13:22:59 -0700

    Create virtualenv using Python 2.7 for drilldown_demo

--- manifests/classes/flask_packages.pp
+++ manifests/classes/flask_packages.pp
@@ -1,11 +1,20 @@
 class flask_packages {
 
+  class { "python::dev":
+    version => "2.7",
+  }
+
+  class { "python::venv":
+    owner => "uwsgi",
+  }
+
   # libevent, for gevent. Note that we can't specify a version (tried), but for
   # Centos 6.4, we should get 1.4.13.
   package { "libevent":
     ensure => "installed"
   }
 
+  # Enable Springdale Linux repo for Python 2.7, 3.3 RPMs
   yumrepo { "springdale":
     baseurl => "http://springdale.math.ias.edu/data/puias/computational/6Server/x86_64/",
     descr => "Springdale Linux Server 6 x86_64",
@@ -18,8 +27,24 @@ class flask_packages {
     require => Yumrepo["springdale"]
   }
 
+  package {"python27-devel":
+    ensure => "installed",
+    require => Yumrepo["springdale"]
+  }
+
   package {"python3":
     ensure => "installed",
     require => Yumrepo["springdale"]
   }
+
+  package {"python3-devel":
+    ensure => "installed",
+    require => Yumrepo["springdale"]
+  }
+
+  python::venv::isolate {
+    "/home/uwsgi/virtualenvs/drilldown_demo":
+      version => "2.7",
+  }
 }
+
