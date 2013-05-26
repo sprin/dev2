e2cc959767399c386be0cb2b9958ec68d69a7f98
sprin <sprin>
2013-05-26 12:37:10 -0700

    Remove unnecessary arch from libevent yum install

--- manifests/classes/flask_packages.pp
+++ manifests/classes/flask_packages.pp
@@ -2,7 +2,7 @@ class flask_packages {
 
   # libevent, for gevent. Note that we can't specify a version (tried), but for
   # Centos 6.4, we should get 1.4.13.
-  package { "libevent.x86_64":
+  package { "libevent":
     ensure => "installed"
   }
 
