b63e3cd910f68bc438f4e5c40358de7d1f6a7b23
sprin <sprin>
2013-05-26 13:22:13 -0700

    Set path in default node

--- manifests/sites.pp
+++ manifests/sites.pp
@@ -1,6 +1,11 @@
 import "classes/*"
 
 node default {
+  # Set up path
+  Exec {
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+  }
+
   include user_uwsgi
   include flask_packages
 }
