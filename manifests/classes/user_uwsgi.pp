93a0767e698be27d4dc1a2c8af8a4fc81e79b9f1
sprin <sprin>
2013-05-26 12:47:18 -0700

    Create /home/uwsgi/virtualenvs/

--- manifests/classes/user_uwsgi.pp
+++ manifests/classes/user_uwsgi.pp
@@ -1,6 +1,13 @@
 class user_uwsgi {
+  # User which hosts all uwsgi apps (eg. Flask).
+
   user { "uwsgi":
     ensure     => "present",
     managehome => true,
+  }
+
+  file { "/home/uwsgi/virtualenvs":
+    ensure => "directory",
+    require => User["uwsgi"],
   }
 }
