ac819ce125e0d396e88c9aa28708ba9d5e850681
sprin <sprin>
2013-05-26 17:39:13 -0700

    Create DB for drilldown demo

--- manifests/classes/drilldown_demo.pp
+++ manifests/classes/drilldown_demo.pp
@@ -4,4 +4,11 @@ class drilldown_demo {
     "/home/uwsgi/virtualenvs/drilldown_demo":
       version => "2.7",
   }
+
+  # Create db for drilldown app.
+  postgresql::db { 'drilldown':
+    user     => 'uwsgi',
+    password => 'uwsgi',
+    grant    => 'all',
+  }
 }
