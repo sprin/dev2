e2e90d64413d7635ea52a1a35f8f5eea7a2fbab2
sprin <sprin>
2013-05-26 17:39:44 -0700

    Use requirements.txt to pip install deps with puppet
    Wow!!

--- manifests/classes/drilldown_demo.pp
+++ manifests/classes/drilldown_demo.pp
@@ -3,6 +3,8 @@ class drilldown_demo {
   python::venv::isolate {
     "/home/uwsgi/virtualenvs/drilldown_demo":
       version => "2.7",
+      # The app must be found in repos/drilldown_demo on the host.
+      requirements => "/media/hosthome/repos/drilldown_demo/requirements.txt",
   }
 
   # Create db for drilldown app.
