0e5b1ab047497a47c70536fc8c597b030375857b
sprin <sprin>
2013-05-26 18:10:06 -0700

    Create softlink to drilldown repo in venv

--- manifests/classes/drilldown_demo.pp
+++ manifests/classes/drilldown_demo.pp
@@ -1,16 +1,21 @@
 class drilldown_demo {
 
+
   python::venv::isolate {
     "/home/uwsgi/virtualenvs/drilldown_demo":
       version => "2.7",
-      # The app must be found in repos/drilldown_demo on the host.
       requirements => "/media/hosthome/repos/drilldown_demo/requirements.txt",
+  }->
+  # The app must be found in repos/drilldown_demo on the host.
+  file {"/home/uwsgi/virtualenvs/drilldown_demo/repo":
+    ensure => "link",
+    target => "/media/hosthome/repos/drilldown_demo",
   }
 
   # Create db for drilldown app.
-  postgresql::db { 'drilldown':
-    user     => 'uwsgi',
-    password => 'uwsgi',
-    grant    => 'all',
+  postgresql::db { "drilldown":
+    user     => "uwsgi",
+    password => "uwsgi",
+    grant    => "all",
   }
 }
